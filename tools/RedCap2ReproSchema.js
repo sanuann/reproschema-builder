264c35fdf2769fbd6a3e5bdaf6367500a81f8a5e
Mike Xiao <mikemxxiao@gmail.com>
2019-12-10 18:56:24 -0500

    Update RedCap2ReproSchema.js
    1. update terms
    2. add functions to build protocol schema and context
    3. add hard code section in beginning of script

--- tools/RedCap2ReproSchema.js
+++ tools/RedCap2ReproSchema.js
@@ -1,3 +1,24 @@
+//User inputs: these are specific to your protocol, fill out before using the script
+
+//1. your protocol name, use underscore for spaces, avoid special characters
+const protocolName = "your_protocol_name"
+
+//2. copy/paste the URL of the github repo where you are hosting the schema files (make sure this is the raw.githubusercontent.com link)
+const yourRepoURL = "https://raw.githubusercontent.com/{userName}/{repoName}/{branchName}/"
+
+//3. add a description to your protocol
+let protocolDescription = "Description for your protocol"
+
+//4. give display names to your activities
+let activityDisplayName = {
+    "yourActivity1": "Activity 1 Display Name",
+    "yourActivity2": "Activity 2 Display Name",
+    //etc
+};
+
+
+
+
 /* ************ Constants **************************************************** */
 const csv = require('fast-csv');
 const fs = require('fs');
@@ -14,11 +35,14 @@ const schemaMap = {
     "Field Label": "question",
     "Field Type": "inputType",
     "Required Field?": "requiredValue",
-    "Text Validation Min": "minValue",
-    "Text Validation Max": "maxValue",
+    "minVal": "schema:minValue",
+    "maxVal": "schema:maxValue",
     "Choices, Calculations, OR Slider Labels": "choices",
-    "Branching Logic (Show field only if...)": "visibility"
+    "Branching Logic (Show field only if...)": "visibility",
+    "multipleChoice": "multipleChoice",
+    "responseType": "@type"
 };
+
 const inputTypeMap = {
     "calc": "number",
     "checkbox": "radio",
@@ -28,27 +52,32 @@ const inputTypeMap = {
 };
 
 const uiList = ['inputType', 'shuffle'];
-const responseList = ['type', 'minValue', 'maxValue', 'requiredValue', 'multipleChoice'];
+const responseList = ['type', 'requiredValue'];
 const defaultLanguage = 'en';
 const datas = {};
+
 /* **************************************************************************************** */
 
 // Make sure we got a filename on the command line.
 if (process.argv.length < 3) {
-    console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+    console.log('Usage: node ' + process.argv[1] + 'your_data_dic.csv');
     process.exit(1);
 }
 // Read the file.
 let csvPath = process.argv[2];
 let readStream = fs.createReadStream(csvPath).setEncoding('utf-8');
 
-let schemaContextUrl = 'https://raw.githubusercontent.com/ReproNim/schema-standardization/master/contexts/generic';
+let schemaContextUrl = 'https://raw.githubusercontent.com/ReproNim/reproschema/master/contexts/generic';
 let order = {};
 let visibilityObj = {};
 let scoresObj = {};
 let blObj = [];
 let languages = [];
 let variableMap = [];
+let protocolVariableMap = [];
+let protocolVisibilityObj = {};
+let protocolOrder = [];
+
 
 let options = {
     delimiter: ',',
@@ -66,17 +95,20 @@ csv
         if (!datas[data['Form Name']]) {
             datas[data['Form Name']] = [];
             // For each form, create directory structure - activities/form_name/items
-            shell.mkdir('-p', 'activities/' + data['Form Name'] + '/items');
+            shell.mkdir('-p', 'activities/' + data['Form Name'] + '/items');          
         }
+        //create directory for protocol
+        shell.mkdir('-p', 'protocols/' + protocolName);
         // console.log(62, data);
         datas[data['Form Name']].push(data);
     })
+
     .on('end', function () {
-        // console.log(66, datas);
-        Object.keys(datas).forEach(form => {
+        //console.log(66, datas);
+        Object.keys(datas).forEach( form => {
             let fieldList = datas[form]; // all items of an activity
             createFormContextSchema(form, fieldList); // create context for each activity
-            let formContextUrl = `https://raw.githubusercontent.com/ReproNim/schema-standardization/master/activities/${form}/${form}_context`;
+            let formContextUrl = `${yourRepoURL}/activities/${form}/${form}_context`;
             scoresObj = {};
             visibilityObj = {};
             variableMap = [];
@@ -86,20 +118,31 @@ csv
                 }
                 processRow(form, field);
             });
-
             createFormSchema(form, formContextUrl);
         });
+            //create protocol context
+            let activityList = Object.keys(datas);
+            let protocolContextUrl = `${yourRepoURL}/protocols/${protocolName}/${protocolName}_context`
+            createProtocolContext(activityList);
+            
+            //create protocol schema
+            activityList.forEach( activityName => {
+            processActivities(activityName);
+            })
+
+            createProtocolSchema(protocolName, protocolContextUrl);
+        
     });
 
 function createFormContextSchema(form, fieldList) {
     // define context file for each form
     let itemOBj = { "@version": 1.1 };
     let formContext = {};
-    itemOBj[form] = `https://raw.githubusercontent.com/ReproNim/schema-standardization/master/activities/${form}/items/`;
+    itemOBj[form] = `${yourRepoURL}/activities/${form}/items/`;
     fieldList.forEach( field => {
         let field_name = field['Variable / Field Name'];
         // define item_x urls to be inserted in context for the corresponding form
-        itemOBj[field_name] = { "@id": `${form}:${field_name}.jsonld` , "@type": "@id" };
+        itemOBj[field_name] = { "@id": `${form}:${field_name}` , "@type": "@id" };
     });
     formContext['@context'] = itemOBj;
     const fc = JSON.stringify(formContext, null, 4);
@@ -110,13 +153,34 @@ function createFormContextSchema(form, fieldList) {
     });
 }
 
+function createProtocolContext(activityList) {
+    //create protocol context file
+    let activityOBj = { "@version": 1.1,
+                    "activity_path": `${yourRepoURL}/activities/`           
+    };
+    let protocolContext = {};
+    activityList.forEach(activity => {
+        //let activityName = activity['Form Name'];
+        // define item_x urls to be inserted in context for the corresponding form
+        activityOBj[activity] = { "@id": `activity_path:${activity}/${activity}_schema` , "@type": "@id" };
+    });
+    protocolContext['@context'] = activityOBj
+    const pc = JSON.stringify(protocolContext, null, 4);
+    fs.writeFile(`protocols/${protocolName}/${protocolName}_context`, pc, function(err) {
+        if (err)
+            console.log(err);
+        else console.log(`Protocol context created for ${protocolName}`);
+    });
+}
+
+
 function processRow(form, data){
     let rowData = {};
     let ui = {};
     let rspObj = {};
     let choiceList = [];
     rowData['@context'] = [schemaContextUrl];
-    rowData['@type'] = 'https://raw.githubusercontent.com/ReproNim/schema-standardization/master/schemas/Field';
+    rowData['@type'] = 'reproschema:Field';
 
     // map Choices, Calculations, OR Slider Labels column to choices or scoringLogic key
     if (data['Field Type'] === 'calc')
@@ -146,6 +210,68 @@ function processRow(form, data){
                 }
             }
 
+            // parse multipleChoice
+            else if (schemaMap[current_key] === 'multipleChoice' && data[current_key] !== '') {
+
+                // split string wrt '|' to get each choice
+                let multipleChoiceVal = (data[current_key]) === '1' ? true:false;
+              
+                // insert 'multiplechoices' key inside responseOptions of the item
+                if (rowData.hasOwnProperty('responseOptions')) {
+                    rowData.responseOptions[schemaMap[current_key]] = multipleChoiceVal;
+                }
+                else {
+                    rspObj[schemaMap[current_key]] = multipleChoiceVal;
+                    rowData['responseOptions'] = rspObj;
+                }
+            }
+          
+            //parse minVal
+            else if (schemaMap[current_key] === 'schema:minValue' && data[current_key] !== '') {
+
+                // split string wrt '|' to get each choice
+                let minValVal = (data[current_key]);
+              
+                // insert 'multiplechoices' key inside responseOptions of the item
+                if (rowData.hasOwnProperty('responseOptions')) {
+                    rowData.responseOptions[schemaMap[current_key]] = minValVal;
+                }
+                else {
+                    rspObj[schemaMap[current_key]] = minValVal;
+                    rowData['responseOptions'] = rspObj;
+                }
+            }
+
+            //parse maxVal
+            else if (schemaMap[current_key] === 'schema:maxValue' && data[current_key] !== '') {
+
+                // split string wrt '|' to get each choice
+                let maxValVal = (data[current_key]);
+              
+                // insert 'multiplechoices' key inside responseOptions of the item
+                if (rowData.hasOwnProperty('responseOptions')) {
+                    rowData.responseOptions[schemaMap[current_key]] = maxValVal;
+                }
+                else {
+                    rspObj[schemaMap[current_key]] = maxValVal;
+                    rowData['responseOptions'] = rspObj;
+                }
+            }
+/*
+            //parse @type
+            else if (schemaMap[current_key] === '@type') {
+
+                // insert "@type":"xsd:anyURI" key inside responseOptions of the item
+                if (rowData.hasOwnProperty('responseOptions')) {
+                    rowData.responseOptions[schemaMap[current_key]] = "xsd:anyURI";
+                }
+                else {
+                    rspObj[schemaMap[current_key]] = "xsd:anyURI";
+                    rowData['responseOptions'] = rspObj;
+                }
+            }
+*/
+
             // parse choice field
             else if (schemaMap[current_key] === 'choices' && data[current_key] !== '') {
 
@@ -157,8 +283,9 @@ function processRow(form, data){
                     let cs = ch.split(', ');
                     // create name and value pair for each choice option
                     choiceObj['schema:value'] = parseInt(cs[0]);
-                    let cnameList = parseHtml(cs[1]);
+                    let cnameList = cs[1];
                     choiceObj['schema:name'] = cnameList;
+                    choiceObj['@type'] = "schema:option";
                     choiceList.push(choiceObj);
 
                 });
@@ -215,11 +342,11 @@ function processRow(form, data){
                     // normalize the condition field to resemble javascript
                     let re = RegExp(/\(([0-9]*)\)/g);
                     condition = condition.replace(re, "___$1");
-                    condition = condition.replace(/([^>|<])=/g, "$1 ==");
+                    condition = condition.replace(/([^>|<])=/g, "$1==");
                     condition = condition.replace(/\ and\ /g, " && ");
                     condition = condition.replace(/\ or\ /g, " || ");
                     re = RegExp(/\[([^\]]*)\]/g);
-                    condition = condition.replace(re, " $1 ");
+                    condition = condition.replace(re, "$1");
                 }
                 visibilityObj[[data['Variable / Field Name']]] = condition;
             }
@@ -260,22 +387,23 @@ function processRow(form, data){
     else order[form].push(field_name);
 
     // write to item_x file
-    fs.writeFile('activities/' + form + '/items/' + field_name + '.jsonld', JSON.stringify(rowData, null, 4), function (err) {
+    fs.writeFile('activities/' + form + '/items/' + field_name, JSON.stringify(rowData, null, 4), function (err) {
         if (err) {
             console.log("error in writing item schema", err);
         }
     });
 }
 
+
 function createFormSchema(form, formContextUrl) {
     // console.log(27, form, visibilityObj);
     let jsonLD = {
         "@context": [schemaContextUrl, formContextUrl],
-        "@type": "https://raw.githubusercontent.com/ReproNim/schema-standardization/master/schemas/Activity",
+        "@type": "reproschema:Activity",
         "@id": `${form}_schema`,
-        "skos:prefLabel": `${form }_schema`,
+        "skos:prefLabel": `${form }`,
         "skos:altLabel": `${form}_schema`,
-        "schema:description": `${form} schema`,
+        "schema:description": `${form}`,
         "schema:schemaVersion": "0.0.1",
         "schema:version": "0.0.1",
         // todo: preamble: Field Type = descriptive represents preamble in the CSV file., it also has branching logic. so should preamble be an item in our schema?
@@ -295,6 +423,47 @@ function createFormSchema(form, formContextUrl) {
         }
         else console.log(form, "Instrument schema created");
     });
+}
+
+function processActivities (activityName) {
+
+    let condition = true; // for items visible by default
+    protocolVisibilityObj[activityName] = condition;
+    
+    // add activity to variableMap and Order
+    protocolVariableMap.push({"variableName": activityName, "isAbout": activityName});
+    protocolOrder.push(activityName);
+
+}
+
+function createProtocolSchema(protocolName, protocolContextUrl) {
+    let protocolSchema = {
+        "@context": [schemaContextUrl, protocolContextUrl],
+        "@type": "reproschema:ActivitySet",
+        "@id": `${protocolName}_schema`,
+        "skos:prefLabel": 'Daily Diary Card',
+        "skos:altLabel": `${protocolName}_schema`,
+        "schema:description": protocolDescription,
+        "schema:schemaVersion": "0.0.1",
+        "schema:version": "0.0.1",
+        // todo: preamble: Field Type = descriptive represents preamble in the CSV file., it also has branching logic. so should preamble be an item in our schema?
+        "variableMap": protocolVariableMap,
+        "ui": {
+            "order": protocolOrder,
+            "shuffle": false,
+            "activity_display_name": activityDisplayName,
+            "visibility": protocolVisibilityObj
+        }
+    };
+    const op = JSON.stringify(protocolSchema, null, 4);
+    // console.log(269, jsonLD);
+    fs.writeFile(`protocols/${protocolName}/${protocolName}_schema`, op, function (err) {
+        if (err) {
+            console.log("error in writing protocol schema")
+        }
+        else console.log("Protocol schema created");
+    });
+
 }
 
 function parseLanguageIsoCodes(inputString){
@@ -333,4 +502,3 @@ function parseHtml(inputString) {
     }
     return result;
 }
-
