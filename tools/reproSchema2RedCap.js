ac048316c0d82c6b58cfb15c4399ca0da0a11a21
Sanu Ann <sanuann@mit.edu>
2020-04-08 22:40:50 -0400

    generates a csv file with column A and B

--- tools/reproSchema2RedCap.js
+++ tools/reproSchema2RedCap.js
@@ -18,47 +18,63 @@ const schemaMap = {
 };
 
 const fs = require('fs');
-var files = fs.readdirSync('./protocols/protocol_name');
-console.log(23, files);
-files.forEach(function (file) {
-    if (file.endsWith('_schema')) {
-        fs.readFile('./protocols/protocol_name' + "/" + file, 'utf8', (err, jsonString) => {
-            if (err) {
-                console.log("File read failed:", err);
-                return
-            }
-            parsedJSON = JSON.parse(jsonString);
-            const activityList = parsedJSON.ui.order;
-            // console.log('File data:', parsedJSON.ui.order);
-            activityList.forEach(function (activity) {
-                fs.readFile(`./activities/${activity}/${activity}_schema`, 'utf8', (err, jsonStr) => {
-                    if (err) {
-                        console.log("Activity schema File read failed:", err);
-                        return
-                    }
-                    parsedJSONActivity = JSON.parse(jsonStr);
-                    console.log(40, activity, '--------', parsedJSONActivity.ui.order);
-                });
-
-            })
-        });
-
-    }
-
+const converter = require('json-2-csv');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+    path: './local_data/protocol_name.csv',
+    header: [
+        {id: 'var_name', title: 'Variable / Field Name'},
+        {id: 'activity', title: 'Form Name'},
+    ]
 });
+let csvData = [];
+var files = fs.readdirSync('./protocols/protocol_name');
+// console.log(23, files);
+files.forEach(function (file) {
+    let p;
+    if (file.endsWith('_schema')) {
+        parsedJSON = JSON.parse(fs.readFileSync('./protocols/protocol_name' + "/" + file, 'utf8'));
 
-function getAllFiles(dirPath, arrayOfFiles) {
-  files = fs.readdirSync(dirPath);
-
-  arrayOfFiles = arrayOfFiles || [];
-
-  files.forEach(function(file) {
-    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
-    } else {
-      arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
     }
-  })
+});
+const activityList = parsedJSON.ui.order;
+csvData = [];
+activityList.forEach(function (activity) {
+    // console.log(43, activity);
+    parsedJSONActivity = JSON.parse(fs.readFileSync(`./activities/${activity}/${activity}_schema`, 'utf8'));
+    (parsedJSONActivity.ui.order).forEach(function (item) {
+        csvData.push({
+          var_name: item,
+          activity: activity
+        });
+    });
+});
+// console.log(57, csvData);
+csvWriter
+.writeRecords(csvData)
+.then(()=> console.log('The CSV file was written successfully'));
 
-  return arrayOfFiles
-}
+
+// converter.json2csvAsync(parsedJSONActivity.ui.order)
+//     .then((csv) => {
+//         console.log(57, csv);
+//     })
+//     .catch((err) => {
+//         console.log(60, err);
+//     });
+
+// function getAllFiles(dirPath, arrayOfFiles) {
+//   files = fs.readdirSync(dirPath);
+//
+//   arrayOfFiles = arrayOfFiles || [];
+//
+//   files.forEach(function(file) {
+//     if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+//       arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+//     } else {
+//       arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
+//     }
+//   })
+//
+//   return arrayOfFiles
+// }
